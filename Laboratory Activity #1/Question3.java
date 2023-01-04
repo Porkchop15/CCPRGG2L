8ff05d44bfe307459b7844666335a275e0388ecd
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2023-01-04 23:48:02 +0800

    Update Question3.java

--- Laboratory Activity #1/Question3.java
+++ Laboratory Activity #1/Question3.java
@@ -5,23 +5,18 @@ import java.io.IOException;
 public class Question3 {
     public static void main(String[]args) throws IOException{
 
-    // Initialize the count from 0
     int vowelCount = 0;
 
-    // Locate and open the file dummy.txt
     try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Z3m\\.vscode\\files\\question 3\\Question3\\dummy.txt"))) {
 
-        // Initialize the count from 0 to 5 in reading the line from the file
         for (int i = 0; i < 5; i++) {
             String text = br.readLine();
 
-            // If the line reaches the line 5 it will stop reading the next lines.
             if (text == null) {
                 
                 break;
             }
 
-            // Counts the vowels in each line
             vowelCount += countVowels(text);
         }
     }
@@ -30,12 +25,9 @@ public class Question3 {
     
     public static int countVowels(String s) {
 
-      // Initialize the vowel count to 0
       int count = 0;
-      // Loop through each character in the string
       for (int i = 0; i < s.length(); i++) {
         char c = s.charAt(i);
-        // If the character is a vowel it will increment the vowel count
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
           count++;
         }
